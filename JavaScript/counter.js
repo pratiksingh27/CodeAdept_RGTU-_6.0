a0aa6c0faef29a345c0701de15d699bf002d9af7
pratiksingh27 <pratik27march@gmail.com>
2022-11-02 20:52:30 +0530

    merging vishal code

--- JavaScript/counter.js
+++ JavaScript/counter.js
@@ -1,10 +1,10 @@
 let valueDisplays = document.querySelectorAll(".num");
-let interval = 5000;
+let interval = 4000;
 
 valueDisplays.forEach((valueDisplay) => {
     let startValue =0;
     let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-    let duration = Math.floor(interval / endValue);
+    let duration = Math.floor(interval/endValue);
     let counter = setInterval(function(){
         startValue += 1;
         valueDisplay.textContent =startValue;
